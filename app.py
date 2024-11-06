2bb2c7e8d0b15d7920cb61a068c728cc7dd7ac1e
ikino89 <151890381+ikino89@users.noreply.github.com>
2024-11-07 06:27:44 +0700

    app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import pickle
-import scikit-learn
+import sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier

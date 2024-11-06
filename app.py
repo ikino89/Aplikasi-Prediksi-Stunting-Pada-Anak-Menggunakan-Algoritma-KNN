197705582b4ffcf351d1dc9d3257b4ddaad4ea21
ikino89 <151890381+ikino89@users.noreply.github.com>
2024-11-06 21:16:33 +0700

    app.py

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 import streamlit as st
 import pandas as pd
 import pickle
-from sklearn.ensemble import KNeighborsClassifierr
-from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
 
 st.title("Aplikasi Prediksi Stunting Anak")
 
 st.sidebar.header("Masukkan Data Anak")
 def user_input_features():
     Umur_bulan = st.sidebar.slider("Umur (bulan)", 0, 60)
-    Jenis_Kelamin = st.sidebar.selectbox("Jenis Kelamin", (1,2))
+    Jenis_Kelamin = st.sidebar.selectbox("Jenis Kelamin", (1, 2))
     Tinggi_Badan_cm = st.sidebar.slider("Tinggi Badan (cm)", 0, 100)
-    Status_Gizi = st.sidebar.selectbox("Status Gizi", (0,1,2,3,))
+    Status_Gizi = st.sidebar.selectbox("Status Gizi", (0, 1, 2, 3))
+    return pd.DataFrame({'Umur_bulan': [Umur_bulan],
+                         'Jenis_Kelamin': [Jenis_Kelamin],
+                         'Tinggi_Badan_cm': [Tinggi_Badan_cm],
+                         'Status_Gizi': [Status_Gizi]})
